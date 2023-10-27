b9b4dbc700064c8f93e229e42fe753be7b5a53de
Darius <dariuskumza@gmail.com>
2023-10-27 21:19:13 +0300

    update styling and some logics

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
-const btn = document.querySelector("#btn");
+window.onload = function () {
+   let minutes = 0;
+   let seconds = 0;
+   let tens = 0;
+   const appendMinutes = document.querySelector("#minutes");
+   const appendTens = document.querySelector("#tens");
+   const appendSeconds = document.querySelector("#seconds");
+   const startBtn = document.querySelector("#start");
+   const stopBtn = document.querySelector("#stop");
+   const resetBtn = document.querySelector("#reset");
+   let interval;
 
-btn.addEventListener("click", () => {
-   const start = new Date().getTime();
-   const end = input.value * 60 * 1000 + start;
-   let timer = setInterval(update, 1000);
+   const startTimer = () => {
+      tens++;
+      if (tens <= 9) {
+         appendTens.textContent = `0${tens}`;
+      }
+      if (tens > 9) {
+         appendTens.textContent = tens;
+      }
 
-   function update() {
-      const current = new Date().getTime();
-      let remaining = (end - current) / 1000;
-      output.textContent = `${Math.floor(remaining / 60)}m ${Math.floor(
-         remaining % 60
-      )}s`;
-   }
-   update();
-});
+      if (tens > 99) {
+         seconds++;
+         appendSeconds.textContent = `0${seconds}`;
+         tens = 0;
+         appendTens.textContent = "00";
+      }
+
+      if (seconds > 9) {
+         appendSeconds.textContent = seconds;
+      }
+
+      if (seconds > 59) {
+         minutes++;
+         appendMinutes.textContent = `0${minutes}`;
+         seconds = 0;
+         appendSeconds.textContent = "00";
+      }
+   };
+
+   startBtn.onclick = () => {
+      clearInterval(interval);
+      interval = setInterval(startTimer, 10);
+   };
+
+   stopBtn.onclick = () => {
+      clearInterval(interval);
+   };
+
+   resetBtn.onclick = () => {
+      clearInterval(interval);
+      appendTens.textContent = "00";
+      appendSeconds.textContent = "00";
+      appendMinutes.textContent = "00";
+   };
+};
