6ef2c1918cd3137ac19e4754c8f2abdb3262c6a7
Darius <dariuskumza@gmail.com>
2023-10-28 11:46:02 +0300

    styling and logic done

--- app.js
+++ app.js
@@ -2,15 +2,65 @@ window.onload = function () {
    let minutes = 0;
    let seconds = 0;
    let tens = 0;
+   let clicked = false;
    const appendMinutes = document.querySelector("#minutes");
    const appendTens = document.querySelector("#tens");
    const appendSeconds = document.querySelector("#seconds");
    const startBtn = document.querySelector("#start");
    const stopBtn = document.querySelector("#stop");
    const resetBtn = document.querySelector("#reset");
+   const controls = document.querySelector(".controls-btn");
    let interval;
 
-   const startTimer = () => {
+   const ifStopClicked = () => {
+      if (clicked) {
+         stopBtn.classList.remove("bi-pause-circle");
+         stopBtn.classList.add("bi-play-circle");
+      } else {
+         stopBtn.classList.add("bi-pause-circle");
+         stopBtn.classList.remove("bi-play-circle");
+      }
+   };
+
+   const startTime = () => {
+      clearInterval(interval);
+      interval = setInterval(stopWatch, 10);
+      startBtn.classList.add("hide");
+      controls.classList.remove("hide");
+      clicked = false;
+   };
+
+   const stopTime = () => {
+      clearInterval(interval);
+      if (!clicked) {
+         clicked = true;
+         ifStopClicked();
+      } else {
+         startTime();
+         clicked = false;
+         ifStopClicked();
+      }
+   };
+
+   const resetTime = () => {
+      clearInterval(interval);
+      minutes = 0;
+      seconds = 0;
+      tens = 0;
+      appendTens.textContent = "00";
+      appendSeconds.textContent = "00";
+      appendMinutes.textContent = "00";
+      startBtn.classList.remove("hide");
+      controls.classList.add("hide");
+      clicked = false;
+      ifStopClicked();
+   };
+
+   startBtn.addEventListener("click", startTime);
+   stopBtn.addEventListener("click", stopTime);
+   resetBtn.addEventListener("click", resetTime);
+
+   const stopWatch = () => {
       tens++;
       if (tens <= 9) {
          appendTens.textContent = `0${tens}`;
@@ -36,21 +86,5 @@ window.onload = function () {
          seconds = 0;
          appendSeconds.textContent = "00";
       }
-   };
-
-   startBtn.onclick = () => {
-      clearInterval(interval);
-      interval = setInterval(startTimer, 10);
-   };
-
-   stopBtn.onclick = () => {
-      clearInterval(interval);
-   };
-
-   resetBtn.onclick = () => {
-      clearInterval(interval);
-      appendTens.textContent = "00";
-      appendSeconds.textContent = "00";
-      appendMinutes.textContent = "00";
    };
 };
